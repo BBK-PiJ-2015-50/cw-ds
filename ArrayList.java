7ff250d318772f99de0d4072cf19270e7991000f
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2015-11-21 03:08:55 +0000

    ArrayList now implements List

--- ArrayList.java
+++ ArrayList.java
@@ -1,4 +1,4 @@
-public class ArrayList /** implements List */ {
+public class ArrayList implements List {
 	
 	private Object[] array;
 
@@ -8,7 +8,7 @@ public class ArrayList /** implements List */ {
 
 	public ArrayList() {
 		array = new Object[INITIAL_ARRAY_SIZE];
-		numberOfItems;
+		int numberOfItems;
 	}
 
 
@@ -21,18 +21,72 @@ public class ArrayList /** implements List */ {
 	}
 
 	public ReturnObject get(int index) {
-
+		ReturnObject check = checkForError(index, size(), isEmpty());
+		if (check.hasError()) {
+			return check;
+		}
+		return new ReturnObjectImpl(array[index]);
 	}
 
 	public ReturnObject remove(int index) {
-
+		ReturnObject check = checkForError(index, size(), isEmpty());
+		if (check.hasError()) {
+			return check;
+		}
+		ReturnObject selectedItem = new ReturnObjectImpl(array[index]);
+		for (int i=index; i<numberOfItems; i++) {
+			array[i] = array[i+1];
+		}
+		array[numberOfItems] = null;
+		numberOfItems--;
+		return selectedItem;
 	}
 
 	public ReturnObject add(int index, Object item) {
-
+		ReturnObject check1 = checkForError(index, size(), isEmpty());
+		if (check1.hasError()) {
+			return check1;
+		}
+		ReturnObject check2 = checkForError(item);
+		if (check2.hasError()) {
+			return check2;
+		}
+		if (numberOfItems == array.length - 1) {
+			doubleArraySize();
+		}
+		for (int i=numberOfItems; i>index; i--) {
+			array[i] = array[i-1];
+		}
+		array[index] = item;
+		numberOfItems++;
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
 	}
 
 	public ReturnObject add(Object item) {
+		return add(numberOfItems, item);
+	}
 
+	private ReturnObject checkForError(int index, int size, boolean empty) {
+		if (index < 0 || index >= size) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (empty) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		}
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+	}
+
+	private ReturnObject checkForError(Object item) {
+		if (item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		}
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+	}
+
+	public void doubleArraySize() {
+		Object[] tempArray = new Object[array.length * 2];
+		for (int i=0; i<numberOfItems; i++) {
+			tempArray[i] = array[i];
+		}
+		array = tempArray;
 	}
 }
